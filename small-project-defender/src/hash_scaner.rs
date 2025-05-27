1ee308bd3206aa410d87694d8d312b904dd1e490
fridalif <fridalif2@gmail.com>
2025-05-27 23:25:41 +0300

    Сканер хешей написан

--- small-project-defender/src/hash_scaner.rs
+++ small-project-defender/src/hash_scaner.rs
@@ -2,6 +2,7 @@ mod appconfig;
 
 use sha2::{Digest, Sha256};
 use std::fs::File;
+use rand::Rng;
 use std::io::prelude::*;
 use std::path::Path;
 
@@ -13,15 +14,15 @@ fn hash_file(file_path: &str) -> Result<String, std::io::Error> {
     Ok(format!("{:x}", hash))
 }
 
-pub fn scan_directory(hash_map: HashMap<String, String>, path: &str) -> HashMap<String, String> {
+fn scan_directory(hash_map: HashMap<String, String>, path: &str, exceptions: Vec<String>) -> (HashMap<String, String>, Vec<String>) {
     let pathRef = Path::new(path);
     if pathRef.is_file() {
         let file_hash = hash_file(path).unwrap();
         hash_map.insert(path.to_string(), file_hash);
-        return hash_map;
+        return (hash_map, exceptions);
     }
     if !pathRef.is_dir() {
-        return hash_map;
+        return (hash_map, exceptions);
     }
 
     let dir = fs::read_dir(path)?;
@@ -29,9 +30,43 @@ pub fn scan_directory(hash_map: HashMap<String, String>, path: &str) -> HashMap<
     for entry in dir {
         let entry = entry?;
         let path = entry.path();
-
-        hash_map = scan_directory(hash_map, &path.to_string());    
+        if exceptions.contains(&path.to_string()) {
+            continue;
+        }
+        hash_map = scan_directory(hash_map, &path.to_string(), exceptions);    
     }
 
-    return hash_map;
+    return (hash_map, exceptions);
+}
+
+pub fn init_origins_hash_scaner(config: appconfig::AppConfig) -> HashMap<String, String> {
+    let mut origins: HashMap<String, String> = HashMap::new();
+    for dir in config.hash_scaner.directories {
+        (origins, exceptions) = scan_directory(origins, &dir, config.hash_scaner.exceptions);
+    }
+    return origins;
+}
+
+pub fn schedule_hash_scaner(origins: HashMap<String, String>, config: appconfig::AppConfig) {
+    let exceptions = config.hash_scaner.exceptions;
+    while true {
+        let temp_cooldown = config.hash_scaner.cooldown;
+        let random_seconds = rand::thread_rng().gen_range(0..temp_cooldown);
+        std::thread::sleep(std::time::Duration::from_secs(random_seconds));
+        let temp_dirs = config.hash_scaner.directories;
+        let new_origins: HashMap<String, String> = HashMap::new();
+        for dir in temp_dirs {
+            (new_origins, exceptions) = scan_directory(origins, &dir, exceptions);    
+        }
+        for (key, value) in new_origins {
+            if !origins.contains(&key) {
+                println!("New file detected: {} with hash: {}", key, value);
+                continue
+            }
+            if origins.get(&key) != Some(&value) {
+                println!("File changed: {} with hash: {}", key, value);
+                continue
+            }
+        }
+    }   
 }
