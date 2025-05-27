a1a622a5893a495eaee6aed83c3caec69b52ada8
fridalif <fridalif2@gmail.com>
2025-05-27 22:19:58 +0300

    Загрузка конфига

--- small-project-defender/src/main.rs
+++ small-project-defender/src/main.rs
@@ -1,7 +1,9 @@
 mod appconfig;
 
-fn main() -> Result<(), Box<dyn std::error::Error>{
-    
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let config = appconfig::AppConfig::new("config.yaml".to_string())?;
+    print!("{:?}", config);
+    Ok(())
 }
 
 
