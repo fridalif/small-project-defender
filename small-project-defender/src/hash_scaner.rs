422daee09088f29649d6a30c2305cce294f271e2
fridalif <fridalif2@gmail.com>
2025-05-27 23:06:08 +0300

    Сделан подсчёт hash'ей в директории

--- small-project-defender/src/hash_scaner.rs
+++ small-project-defender/src/hash_scaner.rs
@@ -13,6 +13,25 @@ fn hash_file(file_path: &str) -> Result<String, std::io::Error> {
     Ok(format!("{:x}", hash))
 }
 
-pub fn scan_directory(config: &appconfig::AppConfig) -> HashMap<String, String> {
-    
+pub fn scan_directory(hash_map: HashMap<String, String>, path: &str) -> HashMap<String, String> {
+    let pathRef = Path::new(path);
+    if pathRef.is_file() {
+        let file_hash = hash_file(path).unwrap();
+        hash_map.insert(path.to_string(), file_hash);
+        return hash_map;
+    }
+    if !pathRef.is_dir() {
+        return hash_map;
+    }
+
+    let dir = fs::read_dir(path)?;
+
+    for entry in dir {
+        let entry = entry?;
+        let path = entry.path();
+
+        hash_map = scan_directory(hash_map, &path.to_string());    
+    }
+
+    return hash_map;
 }
