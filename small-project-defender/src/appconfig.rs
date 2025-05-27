8467537620f46ed0baf9d4322400cfff4f22f357
fridalif <fridalif2@gmail.com>
2025-05-27 22:17:39 +0300

    Загрузка конфига

--- small-project-defender/src/appconfig.rs
+++ small-project-defender/src/appconfig.rs
@@ -20,3 +20,15 @@ pub struct BotConfig {
     admin_chat: String,
 }
 
+impl AppConfig {
+    pub fn new(path: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let yaml_str =  std::fs::read_to_string(&path)
+            .map_err(|e| format!("Failed to read YAML file '{}': {}", path, e))?;
+
+        // Десериализуем YAML в структуру AppConfig
+        let config: AppConfig = serde_yaml::from_str(&yaml_str)
+            .map_err(|e| format!("Failed to parse YAML: {}", e))?;
+
+        Ok(config)
+    }
+}
--- small-project-defender/src/main.rs
+++ small-project-defender/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod appconfig;
+
+fn main() -> Result<(), Box<dyn std::error::Error>{
+    
 }
+
+
+
+
