ea3a74c5bbfd173137d84f1abd98ca34ed2831a2
fridalif <fridalif2@gmail.com>
2025-07-11 10:21:57 +0300

    Работа над обменом сообщениями

--- spd-client/src/connection.rs
+++ spd-client/src/connection.rs
@@ -1,7 +1,14 @@
 use crate::prelude::*;
 use std::sync::{mpsc, Arc, Mutex};
 use std::collections::HashMap;
+use std::net::{TcpListener, TcpStream};
+use tokio::runtime::Runtime;
+
+async fn serve_tcp_client(config: Arc<AppConfig>, rx: mpsc::Receiver<HashMap<String, HashMap<String, String>>>) {
+    
+}
 
 pub fn serve(config: Arc<AppConfig>, rx: mpsc::Receiver<HashMap<String, HashMap<String, String>>>) {
-    return    
+    let rt = Runtime::new().unwrap();
+    rt.block_on(serve_tcp_client(config, rx));
 }
--- spd-client/src/main.rs
+++ spd-client/src/main.rs
@@ -1,20 +1,21 @@
 mod appconfig;
 mod ssh_detector;
-mod hash_scaner;
+//mod hash_scaner;
 mod port_detector;
 mod connection;
 mod prelude {
     pub use crate::appconfig::AppConfig;
 }
 
-use std::sync::{mpsc, Arc, Mutex};
-use std::collections::HashMap;
+use std::sync::{mpsc, Arc}; //Mutex};
+//use std::collections::HashMap;
 use std::thread;
 use crate::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let config = Arc::new(AppConfig::new("../etc/config.yaml".to_string())?);
     let (tx, rx) = mpsc::channel();
+    let mut thread_handles = Vec::new();
     //let mut origins_hash_scaner = Arc::new(Mutex::new(HashMap::new()));
     //if config.hash_scaner.on {
     //    let config_clone = std::sync::Arc::clone(&config);
@@ -26,24 +27,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     if config.ssh_detector.check_auth_on {
         let config_clone = std::sync::Arc::clone(&config);
         let tx = tx.clone();
-        thread::spawn(move || ssh_detector::ssh_auth_log_watcher(std::sync::Arc::clone(&config_clone), tx.clone()));
+        let handle = thread::spawn(move || ssh_detector::ssh_auth_log_watcher(std::sync::Arc::clone(&config_clone), tx.clone()));
+        thread_handles.push(handle);
     }
     if config.ssh_detector.check_journalctl_on {
         let config_clone = std::sync::Arc::clone(&config);
         let tx = tx.clone();
-        thread::spawn(move || ssh_detector::journalctl_watcher(std::sync::Arc::clone(&config_clone), tx.clone()));
+        let handle = thread::spawn(move || ssh_detector::journalctl_watcher(std::sync::Arc::clone(&config_clone), tx.clone()));
+        thread_handles.push(handle);
     }
     if config.port_detector.on {
         let config_clone = std::sync::Arc::clone(&config);
         let tx = tx.clone();
-        thread::spawn(move || port_detector::port_detector(std::sync::Arc::clone(&config_clone), tx.clone()));
+        let handle = thread::spawn(move || port_detector::port_detector(std::sync::Arc::clone(&config_clone), tx.clone()));
+        thread_handles.push(handle);
     }
     {
         //let origins_hash_scaner = std::sync::Arc::clone(&origins_hash_scaner);
         let config_clone = std::sync::Arc::clone(&config);
-        thread::spawn(move || connection::serve(std::sync::Arc::clone(&config_clone), rx));
+        let handle = thread::spawn(move || connection::serve(std::sync::Arc::clone(&config_clone), rx));
+        thread_handles.push(handle);
     }
-
+    for handle in thread_handles {
+        handle.join().unwrap_or_else(|_| ());
+    }
     Ok(())  
 }
 
