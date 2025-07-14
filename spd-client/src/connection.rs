afb91f2ce6a11ff4677e60cfb333e3f1870359a9
fridalif <fridalif2@gmail.com>
2025-07-14 11:03:18 +0300

    Убрана ненуная асинхронность

--- spd-client/src/connection.rs
+++ spd-client/src/connection.rs
@@ -1,27 +1,37 @@
 use crate::{cryptography, prelude::*};
 use std::sync::{mpsc, Arc, Mutex};
 use std::collections::HashMap;
-use tokio::net::TcpStream;
-use tokio::runtime::Runtime;
+use std::net::TcpStream;
 
 
-async fn serve_tcp_client(config: Arc<AppConfig>, rx: mpsc::Receiver<HashMap<String, HashMap<String, String>>>) {
-    let mut stream = match TcpStream::connect(format!("{}:{}", config.socket.host, config.socket.port)).await {
+fn serve_tcp_client(config: Arc<AppConfig>, rx: mpsc::Receiver<HashMap<String, HashMap<String, String>>>) {
+    let mut connected = false;
+    let mut stream  = match TcpStream::connect(format!("{}:{}", config.socket.host, config.socket.port)) {
         Ok(stream) => {
+            connected = true;
             println!("Connected to {}", format!("{}:{}", config.socket.host, config.socket.port));
             stream
         },
         Err(e) => {
+            connected = false;
             eprintln!("Failed to connect to {}: {}", format!("{}:{}", config.socket.host, config.socket.port), e);
-            return;
+            return Err(e.into());
         }
     };
     loop {
-
+        
     }
 }
 
 pub fn serve(config: Arc<AppConfig>, rx: mpsc::Receiver<HashMap<String, HashMap<String, String>>>) {
-    let rt = Runtime::new().unwrap();
-    rt.block_on(serve_tcp_client(config, rx));
+    let thread_handlers = !vec();
+    {
+        let config = config.clone();
+        let rx = rx.clone();
+        let handle = thread::spawn(move || serve_tcp_client(config, rx));
+        thread_handlers.push(handle);
+    }
+    for handle in thread_handlers {
+        handle.join().unwrap();
+    }
 }
